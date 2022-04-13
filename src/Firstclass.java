a86759067c7a2046a45dfa635c87da43ccad7998
Ashokgit099 <47042444+Ashokgit099@users.noreply.github.com>
2022-04-13 19:24:27 -0400

    adding file to the system

---
+++ src/Firstclass.java
@@ -0,0 +1,15 @@
+public class Firstclass {
+
+    public static void main (String [] args){
+        System.out.println("Anjana Lamichhane");
+        System.out.println("Northcarolina");
+        System.out.println("Nepal");
+        System.out.println("QA Automation training");
+        System.out.println("IT career");
+        System.out.println("business and finance background");
+        System.out.println("cooking and gardening best hobby");
+        System.out.println("it is 80s out hot in here");
+        System.out.println("Java Learning is fun");
+        System.out.println("have a good day");
+    }
+}
